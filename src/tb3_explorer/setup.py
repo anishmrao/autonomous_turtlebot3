6abea4c2c273d5a9552356a3b67a7ed860023593
Anish M Rao <anish01123@gmail.com>
2025-08-27 20:24:37 +0800

    Updated info and paths

--- src/tb3_explorer/setup.py
+++ src/tb3_explorer/setup.py
@@ -9,15 +9,13 @@ setup(
     version='0.0.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='You',
-    maintainer_email='you@example.com',
-    description='RRT* Global Planner for Nav2 with TB3',
+    maintainer='Anish',
+    maintainer_email='anish01123@gmail.com',
+    description='RRT* Global Planner, frontier explorer, and semantic query support for Nav2 with TB3',
     license='Apache-2.0',
     entry_points={
         'console_scripts': [
